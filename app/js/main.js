857a124fe70572844965f75750341a8065b56ad7
fangbo <wfb_01@163.com>
2015-12-31 15:15:27 +0800

    gulp-demo

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,6 @@
 (function(window,undefined){
     function add(a,b){
-    	a=a+c;
     	return a + b;
     }
-    
     add(10,100);
 })(window);
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,31 @@
 /*加载模块*/
 var gulp = require('gulp'),
+minifyhtml = require('gulp-minify-html'),
 uglify = require('gulp-uglify'),
 minifycss = require('gulp-minify-css'),
 imagemin = require('gulp-imagemin'),
 jshint = require('gulp-jshint'),
-concat = require('gulp-concat')
+csslint = require('gulp-csslint'),
+concat = require('gulp-concat'),
 rename = require('gulp-rename'),
 clean = require('gulp-clean'),
 notify = require('gulp-notify'),
+connect = require('gulp-connect'),
+watch = require('gulp-watch'),
+pngquant = require('imagemin-pngquant'),
+rev = require('gulp-rev');
 del = require('del');
 
+/**************************打包发布部分**************************/
+/*打包发布*/
+gulp.task('build',['clean:build','minifyhtml','minifycss','jshint','minifyjs','minifyimgg']);
+
+/*压缩HTML*/
+gulp.task('minifyhtml',function(){
+  gulp.src('**/*.html',{cwd:'app/views'})
+  .pipe(minifyhtml())
+  .pipe(gulp.dest('build/views'))
+});
 /*压缩js文件*/  
 gulp.task('minifyjs',function(){
   gulp.src(['**/*.js','!**/*.min.js'],{cwd:'app/js'})
@@ -20,8 +36,20 @@ gulp.task('minifyjs',function(){
 
 /*压缩img*/
 gulp.task('minifyimg',function(){
-   gulp.src('**/*',{cwd:'e:/svn_projects/ShopNC/UI/Static_pages/shop/images'})
+   gulp.src('**/*',{cwd:'app/img'})
   .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+  .pipe(gulp.dest('build/img'))
+  /*.pipe(notify({ message: 'Images task complete' }));*/
+});
+
+/*深度压缩img*/
+gulp.task('minifyimgg',function(){
+   gulp.src('**/*',{cwd:'app/img'})
+  .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],//不要移除svg的viewbox属性
+            use: [pngquant()] //使用pngquant深度压缩png图片的imagemin插件
+        }))
   .pipe(gulp.dest('build/img'))
   /*.pipe(notify({ message: 'Images task complete' }));*/
 });
@@ -34,11 +62,18 @@ gulp.task('minifycss',function(){
  .pipe(gulp.dest('build/css'))
 });
 
-/*js 语法检查*/
+/*js 检查*/
 gulp.task('jshint',function(){
   gulp.src('**/*.js',{cwd:'app/js'})
   .pipe(jshint('.jshintrc'))
   .pipe(jshint.reporter('default'))
+});
+
+/*css 检查*/
+gulp.task('csslint',function(){
+ gulp.src('**/*.css',{cwd:'E:/svn_projects/ShopNC/UI/Static_pages/shop/css'})
+ .pipe(csslint())
+ .pipe(csslint.reporter())
 });
 
 /*文件合并*/
@@ -56,6 +91,12 @@ gulp.task('watch',function(){
   })
 });
 
+/*添加版本号*/
+gulp.task('rev',function(){
+   gulp.src('**/*.css',{cwd:'app/css'})
+   .pipe(rev())
+   .pipe(gulp.dest('build/css'))
+});
 /*清空文件、文件夹*/
 gulp.task('clean',function(){
  gulp.src(['build/js/*','build/css/*','build/img/*'], {read: false})
@@ -63,9 +104,34 @@ gulp.task('clean',function(){
 })
 
 /*删除文件*/
-gulp.task('del',function(){
-  del(['build/js/','build/img/','build/css/'])
+gulp.task('clean:build',function(){
+  del.sync('build/*',{force:true})
 });
 
+/*默认任务*/
+gulp.task('default', function() {
+    gulp.start('minifyjs', 'minifycss', 'minifyimg');
+});
+
+/**************************启动服务测试部分**************************/
+/*启动服务*/
+gulp.task('start',['server','watchhtml'])
+/*创建服务*/
+gulp.task('server',function(){
+   connect.server({
+    root:'app',
+    port:9000,
+    livereload: true
+   });
+});
+/*监测文件变化自动刷新*/
+gulp.task('watchhtml',function(){
+   gulp.watch('**/*.html',{cwd:'app/views'},['html'])
+})
+
+gulp.task('html', function () {
+  gulp.src('**/*.html',{cwd:'app/views'})
+    .pipe(connect.reload());
+});
 
 
